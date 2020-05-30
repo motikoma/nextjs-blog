b1d71287c390479681438f4f6107c180372a0ec6
motikoma <motikoma5125@gmail.com>
2020-05-30 15:10:22 +0900

    TypeScript化

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -3,15 +3,21 @@ import Layout from "../../components/layout"
 import {getAllPostIds, getPostData} from "../../lib/posts"
 import Date from "../../components/date"
 import utilStyles from '../../styles/utils.module.css'
+import {GetStaticProps, GetStaticPaths} from "next";
+import { ParsedUrlQuery } from "querystring"
 
-export default function Post({postData}){
+export default function Post({postData}: {postData:{
+    title: string;
+    date: string;
+    contentHtml: string;
+}}){
     return (
         <Layout>
             <Head>
                 <title>{postData.title}</title>
             </Head>
             <article>
-                <h1 className={utilStyles.headingXl}>{postData.id}</h1>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
                 <div className={utilStyles.lightText}>
                     <Date dateString={postData.date} />
                 </div>
@@ -21,16 +27,16 @@ export default function Post({postData}){
     );
 }
 
-export async function getStaticPaths(){
-    const paths = getAllPostIds()
+export const getStaticPaths: GetStaticPaths = async () => {
+    const paths = await getAllPostIds()
     return {
         paths,
         fallback: false
     }
 }
 
-export async function getStaticProps({params}){
-    const postData = await getPostData(params.id)
+export const getStaticProps: GetStaticProps = async ({params}) => {
+    const postData = await getPostData(params.id as string)
     return {
         props: {
             postData
